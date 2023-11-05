f304a70fc676b138f3dde2ec2d3200b7f2a0a4a8
HuBaMarin <marinlosenlaces@gmail.com>
2023-11-05 12:11:00 +0100

    ingreds modif

--- Ingredientes.js
+++ Ingredientes.js
@@ -15,6 +15,11 @@ class Ingredientes extends Hamburguesas {
         mostrar.addEventListener("click", funcion, false);
         mostrar.nombre = this.nombresArray;
 
+        function funcion(params) {
+            for (var i = 0; i < params.currentTarget.nombre.length; i++) {
+                console.log(`${products[i].name} - ${products[i].price}`);
+              }
+        }
         document.getElementById("verPreciosIngreds").innerHTML = precios;
         
     }
